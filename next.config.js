59711c9151887efba3e4319cc7869d5a99c02b69
Denno31 <denniskyn80@gmail.com>
2024-01-18 20:02:18 +0300

    added configurations for next js styled components

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  styledComponents: {
+    // Enable display of the component name along with the generated className (needed for debugging).
+    displayName: true,
+    // Enable SSR support
+    ssr: true,
+    // Optional
+    fileName: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
