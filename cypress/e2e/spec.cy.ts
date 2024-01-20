29e6ff6a74e238657aa4b21fd8f3cacc080f8baa
Denno31 <denniskyn80@gmail.com>
2024-01-20 03:00:28 +0300

    added test to check whether conic gradient is calculated correctly

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,3 +1,5 @@
+import { getConicGradientDeg } from '../../utils/utils';
+
 describe('FeedsSectionMatchesHeader component', () => {
     it('highlights the active filter button', () => {
         cy.visit('http://localhost:3000');
@@ -28,7 +30,7 @@ describe('FeedsSectionMatchesHeader component', () => {
 
 describe('MatchProgress component', () => {
     it('renders without errors', () => {
-        cy.visit('http://localhost:3000'); // Adjust the URL as needed
+        cy.visit('http://localhost:3000');
         cy.get('[data-testid="match-progress"]').should('exist');
     });
 });
@@ -37,5 +39,33 @@ describe('MatchProgress component', () => {
     it('applies conic gradient based on match status', () => {
         cy.visit('http://localhost:3000');
         cy.get('[data-testid="outer-box"]').should('have.css', 'background').and('include', 'conic-gradient');
+    });
+});
+
+describe('getConicGradientDeg function', () => {
+    it('calculates conic gradient degree correctly for in-progress status', () => {
+        const liveStatusType = 'inprogress';
+        const liveStatus = '45';
+
+        const result = getConicGradientDeg(liveStatusType, liveStatus);
+
+        expect(result).to.equal((Number(liveStatus) * 360) / 90);
+    });
+
+    it('calculates conic gradient degree correctly for finished status', () => {
+        const liveStatusType = 'finished';
+        const liveStatus = undefined;
+
+        const result = getConicGradientDeg(liveStatusType, liveStatus);
+        expect(result).to.equal(360);
+    });
+
+    it('calculates conic gradient degree correctly for default status', () => {
+        const liveStatusType = undefined;
+        const liveStatus = undefined;
+
+        const result = getConicGradientDeg(liveStatusType, liveStatus);
+
+        expect(result).to.equal(0);
     });
 });
