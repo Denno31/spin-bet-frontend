291901ea197efc9e750a02abd0403161da420325
Denno31 <denniskyn80@gmail.com>
2024-01-22 12:58:47 +0300

    removed console.log

--- utils/utils.ts
+++ utils/utils.ts
@@ -55,9 +55,7 @@ export const getConicGradientDeg = (liveStatusType: string | undefined, liveStat
 };
 
 export const getTextToStatusToShowOnScoreboard = (match: Match | null | undefined) => {
-    console.log(match);
     if (!match) return '';
-    const liveStatus = parseInt(match.liveStatus, 10);
     if (match.status.type === 'inprogress') {
         return 'LIVE';
     } else if (match.status.type === 'finished') {
